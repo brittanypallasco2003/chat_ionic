215772a143423315dfab9e37174741815e50b52e
brittanypallasco2003 <nohemiespinel0@gmail.com>
2024-06-13 16:18:00 -0500

    creación de páginas y del servicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,15 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'chat',
+    loadChildren: () => import('./pages/chat/chat.module').then( m => m.ChatPageModule)
+  },
+
 ];
 
 @NgModule({
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAwH7ERJBK12Q3zar5q-EHLUJl7Scmtw8g',
+    authDomain: 'chatbot-ionic-5c561.firebaseapp.com',
+    projectId: 'chatbot-ionic-5c561',
+    storageBucket: 'chatbot-ionic-5c561.appspot.com',
+    messagingSenderId: '347306621194',
+    appId: '1:347306621194:web:ad87a033b91774578fa2f3',
+    measurementId: 'G-FTG9LNTT80',
+  },
 };
 
 /*
