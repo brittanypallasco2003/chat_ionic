cfdb4e0147d77cb9fd73c324f7a5b88e50e9ab13
brittanypallasco2003 <nohemiespinel0@gmail.com>
2024-06-15 01:02:44 -0500

    guardar fotos en storage

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,12 +6,12 @@ export const environment = {
   production: false,
   firebaseConfig: {
     apiKey: "AIzaSyAwH7ERJBK12Q3zar5q-EHLUJl7Scmtw8g",
-    authDomain: "chatbot-ionic-5c561.firebaseapp.com",
-    projectId: "chatbot-ionic-5c561",
-    storageBucket: "chatbot-ionic-5c561.appspot.com",
-    messagingSenderId: "347306621194",
-    appId: "1:347306621194:web:ad87a033b91774578fa2f3",
-    measurementId: "G-FTG9LNTT80"
+  authDomain: "chatbot-ionic-5c561.firebaseapp.com",
+  projectId: "chatbot-ionic-5c561",
+  storageBucket: "chatbot-ionic-5c561.appspot.com",
+  messagingSenderId: "347306621194",
+  appId: "1:347306621194:web:ad87a033b91774578fa2f3",
+  measurementId: "G-FTG9LNTT80"
   },
 };
 
